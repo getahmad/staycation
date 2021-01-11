b8ff7123c79c1649a8ecb05233d9a3d3db88555a
Ahmad Danang <ahmad.danang91@gmail.com>
2021-01-11 13:06:04 +0700

    finished checkout page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Checkout from "pages/Checkout";
 import DetailsPage from "pages/DetailsPage";
 import LandingPage from "pages/LandingPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
+        <Route exact path="/checkout" component={Checkout} />
       </Router>
     </div>
   );
